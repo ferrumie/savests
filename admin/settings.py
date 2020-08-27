4641f9e7f79c6ceabe5828d2270e9a2642b0d256
Alabi OluwaFeranmi <alabiemmanuelferanmi@gmail.com>
2020-08-27 04:06:36 +0100

    whitenoise

--- admin/settings.py
+++ admin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#!6c$elt%xy$h+3f@q3u2!vm8y=9#+bd-jio3qnddg^)03ss=+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
