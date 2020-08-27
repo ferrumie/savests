e30d190b129ebf342876d120b60abac47bf0d26c
Alabi OluwaFeranmi <alabiemmanuelferanmi@gmail.com>
2020-08-27 03:54:13 +0100

    profile

--- admin/settings.py
+++ admin/settings.py
@@ -128,4 +128,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
