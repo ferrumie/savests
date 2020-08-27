80f816adfa6434adc6b707570c96f91548436579
Alabi OluwaFeranmi <alabiemmanuelferanmi@gmail.com>
2020-08-27 03:59:00 +0100

    whitenoise

--- admin/urls.py
+++ admin/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.urls import path, include
 from adminapp.views import SendUserEmails
 from adminapp.admin import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', admin.urls, name="admin"),
     path("email-users/", view=SendUserEmails.as_view(), name='email')
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
